181d120995f8cd3c6577a9b0b51c55d73ee197d7
isrand <isrand@protonmail.com>
2023-12-19 18:46:47 +0100

    Add Kyber.ts unit tests

--- lib/crypto/interfaces/ICryptoAdapter.d.ts
+++ lib/crypto/interfaces/ICryptoAdapter.d.ts
@@ -4,7 +4,7 @@ import { ITopicEncryptionKeyAndInitVector } from "../../hedera/interfaces/ITopic
 import { ITopicData } from "../../hedera/interfaces/ITopicData";
 import { IKeyPair } from "./IKeyPair";
 export interface ICryptoAdapter {
-    generateKeyPair(): IKeyPair | never;
+    generateKeyPair(): IKeyPair;
     getEncryptedTopicKeysObject(topicEncryptionKey: Buffer, topicEncryptionInitVector: Buffer, publicKeys: string[]): IEncryptedTopicKeysObject;
     symmetricEncrypt(data: string, symmetricKey: Buffer, initVector: Buffer): string;
     symmetricDecrypt(data: string, symmetricKey: Buffer, initVector: Buffer): string;
